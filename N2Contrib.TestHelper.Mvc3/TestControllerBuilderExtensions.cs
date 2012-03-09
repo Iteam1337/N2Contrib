717c7ccc4ef2668d930bf4cc0f3a1f78999b9fe1
Markus Ullmark <ullmark@gmail.com>
2012-03-09 09:56:33 +0100

    continued with the mvc integration

---
+++ N2Contrib.TestHelper.Mvc3/TestControllerBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MvcContrib.TestHelper;
+using N2.Web.Mvc;
+
+namespace N2Contrib.TestHelper
+{
+    /// <summary>
+    /// Extensions for the Test Controller Builder 
+    /// </summary>
+    public static class TestControllerBuilderExtensions
+    {
+        /// <summary>
+        /// Initializes a Content Controller
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="controller"></param>
+        public static void InitializeContentController(this TestControllerBuilder builder, ContentController controller)
+        {
+            builder.InitializeController(controller); 
+        }
+    }
+}
