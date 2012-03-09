ac3a6ebd76074d4e5b73988736d70acd02459e6b
Markus Ullmark <ullmark@gmail.com>
2012-03-09 09:34:40 +0100

    Rename to mv3

---
+++ N2Contrib.TestHelper.Mvc3/N2ControllerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MvcContrib.TestHelper;
+using N2.Web.Mvc;
+
+namespace N2Contrib.TestHelper
+{
+    public static class N2ControllerExtensions
+    {
+        public static void InitializeN2Controller(this TestControllerBuilder builder, ContentController controller)
+        {
+            
+        }
+    }
+}
