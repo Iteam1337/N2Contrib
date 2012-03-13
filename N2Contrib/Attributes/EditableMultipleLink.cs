1cc3524462831615ce4f2f55b6461dae399d0a80
Kristian Laustsen <kristian@laustsen.se>
2012-03-13 09:37:17 +0100

    Added EditableMultipleLink attribute and some references. :sparkles: :heart: :sparkles:

---
+++ N2Contrib/Attributes/EditableMultipleLink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using N2.Details;
+
+namespace N2Contrib.Attributes
+{
+    class EditableMultipleLink : AbstractEditableAttribute
+    {
+        protected override System.Web.UI.Control AddEditor(System.Web.UI.Control container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateEditor(N2.ContentItem item, System.Web.UI.Control editor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UpdateItem(N2.ContentItem item, System.Web.UI.Control editor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
