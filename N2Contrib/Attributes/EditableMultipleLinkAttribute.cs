4d931759813a26b992e7683a5ff19fac9c294de1
Kristian Laustsen <kristian@laustsen.se>
2012-03-13 09:58:49 +0100

    Changed name on attribute and added some initial documentation

---
+++ N2Contrib/Attributes/EditableMultipleLinkAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.UI;
+using N2;
+using N2.Details;
+
+namespace N2Contrib.Attributes
+{
+    /// <summary>
+    /// Defines n editable links to other items on this site. The items are 
+    /// selected through a popup window displaying the item tree.
+    /// </summary>
+    /// <example>
+    ///		[EditableMultipleLink("Feed items", 90)]
+    ///		public virtual ContentItem Feeds { get; set; }
+    /// </example>
+    public class EditableMultipleLinkAttribute : AbstractEditableAttribute
+    {
+        public EditableMultipleLinkAttribute() : this(null, 100)
+		{}
+
+        public EditableMultipleLinkAttribute(string title, int sortOrder) : base(title, sortOrder)
+		{}
+
+        protected override Control AddEditor(Control container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateEditor(ContentItem item, Control editor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UpdateItem(ContentItem item, Control editor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
