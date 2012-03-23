df3b655887573b4630f2c6b345f7c97eac136c7e
Cristian Libardo <cristianlibardo@hotmail.com>
2012-03-23 14:05:27 +0100

    Moved assembly version to shared file

--- N2Contrib.Mvc3/Properties/AssemblyInfo.cs
+++ N2Contrib.Mvc3/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c8f8ca4a-db41-4e20-9b06-8c2b44eccf9a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- N2Contrib.TestHelper.Mvc3/Properties/AssemblyInfo.cs
+++ N2Contrib.TestHelper.Mvc3/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("99fb64e6-46bc-4ef7-bb2a-975d85f4b619")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.10")]
-[assembly: AssemblyFileVersion("0.1.1.10")]
